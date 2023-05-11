06875b8bff9b0a71bd755ebf44bc25741c6a48f6
aditya121207 <107939866+aditya121207@users.noreply.github.com>
2023-05-11 13:59:07 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyBCSLmVNHoH6-EGhGrDW6CYnAKWs84EX1k",
+  authDomain: "storytellingapp-c5fc2.firebaseapp.com",
+  projectId: "storytellingapp-c5fc2",
+  storageBucket: "storytellingapp-c5fc2.appspot.com",
+  messagingSenderId: "981340151035",
+  appId: "1:981340151035:web:8be38ba3d9b174e822e45e"
+};
+
